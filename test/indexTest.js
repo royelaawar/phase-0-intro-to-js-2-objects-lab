94c8834d734474685c8902ab7a6a5692cc3b959b
Roy Elaawar <roy.elaawar@gmail.com>
2023-09-10 15:22:00 -0400

    objects lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -8,6 +8,7 @@ describe('employees', function() {
       }
 
       employee.name = 'Sam';
+      
     });
 
     it('returns an employee with the original key value pairs and the new key value pair', function () {
